9eebc4070f87be523b9f874e963cb37f9fdfac16
Pie Dev <jimipulsar@gmail.com>
2023-02-07 13:33:05 +0100

    seo title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5">
     <meta name="robots" content="noindex, nofollow">
-    <title>@yield('title') | Livewire Ecommerce</title>
+    <title>Livewire E-commerce Platform </title>
     <meta name="description" content="@yield('description')">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="canonical" href="{{url()->current()}}"/>
