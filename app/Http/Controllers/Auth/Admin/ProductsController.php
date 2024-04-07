631338e41b602e5b8bc2d518748b3196e94e5e4d
jimipulsar <jimipulsar@gmail.com>
2024-04-07 23:44:47 +0200

    product details field required

--- app/Http/Controllers/Auth/Admin/ProductsController.php
+++ app/Http/Controllers/Auth/Admin/ProductsController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Attribute;
 use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
--- resources/views/livewire/search-attribute.blade.php
+++ resources/views/livewire/search-attribute.blade.php
@@ -42,17 +42,17 @@
                         class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                         Immagine
                     </th>
-                    <th style="width:200px; cursor:pointer" wire:click.prevent="sortBy('item_name')"
+                    <th style="width:200px; cursor:pointer" wire:click.prevent="sortBy('name')"
                         class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                         Nome Attributo
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="{{ $sortColumnName === 'item_name' && $sortDirection === 'asc' ? 'black' : 'currentColor' }}"
+                             stroke="{{ $sortColumnName === 'name' && $sortDirection === 'asc' ? 'black' : 'currentColor' }}"
                              class="w-3 h-3 inline-block">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                   d="M8.25 6.75L12 3m0 0l3.75 3.75M12 3v18"/>
                         </svg>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="{{ $sortColumnName === 'item_name' && $sortDirection === 'desc' ? 'black' : 'currentColor' }}"
+                             stroke="{{ $sortColumnName === 'name' && $sortDirection === 'desc' ? 'black' : 'currentColor' }}"
                              class="w-3 h-3 inline-block">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                   d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3"/>
@@ -60,6 +60,7 @@
                     </th>
                     <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                         Relazione
+
                     </th>
                     <th style="width:300px"
                         class="pr-20 px-6 py-3 border-b border-gray-200 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
@@ -99,8 +100,8 @@
                         </td>
 
                         @if($item->parentAttribute)
-                            <td class="text-sm px-6 py-4 whitespace-no-wrap border-b border-gray-200">{{ $item->name }}
-                                <strong>-></strong> {{ $item->parentAttribute->name }}</td>
+                            <td class="text-sm px-6 py-4 whitespace-no-wrap border-b border-gray-200"> {{ $item->parentAttribute->name }}
+                                <strong>-></strong> {{ $item->name }}</td>
                         @else
                             <td class="text-sm px-6 py-4 whitespace-no-wrap border-b border-gray-200">{{ $item->name }}</td>
                         @endif
