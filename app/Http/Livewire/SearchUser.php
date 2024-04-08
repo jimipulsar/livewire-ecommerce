edfa044937e53474d46641afc7d7a0489f1abdb2
jimipulsar <jimipulsar@gmail.com>
2024-04-08 17:54:15 +0200

    search customer component

---
+++ app/Http/Livewire/SearchUser.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Customer;
+use App\Models\User;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class SearchUser extends Component
+{
+    use WithPagination;
+
+//    protected $paginationTheme = 'bootstrap';
+
+    public $searchUsers;
+    public $perPage = 10;
+    public $filters = [];
+    public $sort = 'created_at|desc';
+    public $sortColumnName = 'created_at';
+    public $sortDirection = 'desc';
+    public $field;
+
+    /*
+     * Reset pagination when doing a searchTerm
+     */
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function render()
+    {
+        $users = Customer::with('orders')->withCount('orders');
+
+        $this->applySearchFilter($users->orderBy($this->sortColumnName, $this->sortDirection));
+
+
+        $users = $users->orderBy($this->sortByColumn(), $this->sortDirection())
+            ->paginate($this->perPage);
+
+        return view('livewire.search-user', [
+            'users' => $users
+        ]);
+    }
+
+    public function updateOrder($list)
+    {
+
+        foreach ($list as $itemsAttribute) {
+
+            Customer::find((int)$itemsAttribute['value'])->update(['id' => (int)$itemsAttribute['order']]);
+        }
+
+
+    }
+
+
+    public function sortByColumn()
+    {
+        $sort = explode("|", $this->sort);
+
+        if (!$sort[0]) {
+            return;
+        }
+
+        return $sort[0];
+    }
+
+    public function sortDirection()
+    {
+        $sort = explode("|", $this->sort);
+
+        return $sort[1] ?? 'asc';
+    }
+
+    private function applySearchFilter($users)
+    {
+        if ($this->searchUsers) {
+            return $users->whereRaw("billing_name LIKE \"%$this->searchUsers%\"")
+                ->orWhereRaw("billing_surname LIKE \"%$this->searchUsers%\"")
+                ->orWhereRaw("email LIKE \"%$this->searchUsers%\"");
+
+        }
+
+        return null;
+    }
+
+
+    public function sortBy($columnName)
+    {
+        if ($this->sortColumnName === $columnName) {
+            $this->sortDirection = $this->swapSortDirection();
+        } else {
+            $this->sortDirection = 'asc';
+        }
+
+        $this->sortColumnName = $columnName;
+    }
+
+    public function swapSortDirection()
+    {
+        return $this->sortDirection === 'asc' ? 'desc' : 'asc';
+    }
+}
---
+++ resources/views/livewire/search-user.blade.php
@@ -0,0 +1,108 @@
+<div class="flex flex-col mt-3">
+    <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+
+        <div style="width: 100% !important;max-width: 450px">
+            <div class="search-style-2 my-4">
+                <form>
+                    <label for="default-search"
+                           class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+                    <div class="relative">
+                        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor"
+                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                            </svg>
+                        </div>
+                        <input wire:model="searchUsers" type="search" id="default-search"
+                               class="block p-4 pl-10 w-full text-sm text-gray-900 bg-white-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                               placeholder="Cerca Utente..." required>
+                        {{--                        <button type="submit" class="btn px-6  py-2.5 bg-blue-600 hover:bg-blue-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-blue-900  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out  absolute right-2.5 bottom-2.5">Cerca</button>--}}
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="my-4">
+            <p class="text-sm text-gray-700 leading-5">
+                <span>{!! __('Showing') !!}</span>
+                <span class="font-medium">{{ $users->firstItem() }}</span>
+                <span>{!! __('to') !!}</span>
+                <span class="font-medium">{{ $users->lastItem() }}</span>
+                <span>{!! __('of') !!}</span>
+                <span class="font-medium">{{ $users->total() }}</span>
+                <span>{!! __('results') !!}</span>
+            </p>
+        </div>
+        <div
+            class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+            <table class="min-w-full">
+                <thead>
+                <tr>
+                    <th wire:click.prevent="sortBy('billing_name')"
+                        class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                        Nome e Cognome
+                    </th>
+                    <th style="width:200px; cursor:pointer" wire:click.prevent="sortBy('email')"
+                        class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                        E-mail
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="{{ $sortColumnName === 'email' && $sortDirection === 'asc' ? 'black' : 'currentColor' }}"
+                             class="w-3 h-3 inline-block">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M8.25 6.75L12 3m0 0l3.75 3.75M12 3v18"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="{{ $sortColumnName === 'email' && $sortDirection === 'desc' ? 'black' : 'currentColor' }}"
+                             class="w-3 h-3 inline-block">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3"/>
+                        </svg>
+                    </th>
+                    <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                        Indirizzo
+                    </th>
+                    <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                        Telefono
+                    </th>
+                    <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                        Città
+                    </th>
+                </tr>
+                </thead>
+
+                <tbody class="bg-white">
+                @foreach ($users as $user)
+                    <tr>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <div
+                                class="leading-5 text-gray-900 ">{{ $user->billing_name }} {{ $user->billing_surname }}</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <div
+                                class="leading-5 text-gray-900 ">{{ $user->email }}</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <div
+                                class="leading-5 text-gray-900 ">{{ $user->billing_address }}</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <div
+                                class="leading-5 text-gray-900 ">{{ $user->billing_phone }}</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <div class="text-sm leading-5 text-gray-900"> {{ $user->billing_city }}</div>
+
+                        </td>
+
+                    </tr>
+                @endforeach
+
+                </tbody>
+            </table>
+        </div>
+        <div class="d-flex justify-content-center mt-5 mb-5">
+            {{ $users->links('vendor.pagination.tailwind') }}
+
+        </div>
+    </div>
+</div>
